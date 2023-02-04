4b8f9e6bda1dcd682c754665dddac8471cbfb4c5
Yordan K. Pachev <pachev.yordan@gmail.com>
2023-02-04 23:05:40 +0200

    Console Game GuessANumber
    A new simple console game and also my second project

---
+++ GuessANumber/GuessANumber/GuessANumber.cs
@@ -0,0 +1,32 @@
+﻿
+Random randomNumber = new Random();
+int computerNumber = randomNumber.Next(1, 101);
+
+while (true)
+{
+    Console.Write("Guess a number (1-100): ");
+
+    string playerMove = Console.ReadLine()!;
+    bool isValid = int.TryParse(playerMove, out int playerNumber);
+
+    if (isValid)
+    {
+        if(playerNumber == computerNumber)
+        {
+            Console.WriteLine("You guessed it!");
+            break;
+        }
+        else if(playerNumber > computerNumber) 
+        {
+            Console.WriteLine("Too high");
+        }
+        else
+        {
+            Console.WriteLine("Too low");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input.");
+    }
+}
